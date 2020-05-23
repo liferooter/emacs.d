002c9ce99e963dafc0588bab60e2bb6d552506e4
Liferooter <glebsmirnov0708@gmail.com>
2020-05-23 19:30:55 +0300

    Yet another update

--- lisp/my_packages.el
+++ lisp/my_packages.el
@@ -37,12 +37,9 @@
 
 ;; Helm
 (require 'helm-config)
-(global-set-key (kbd "M-x") 'helm-M-x)
+; (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-
-;; Company
-(require 'company)
 
 
 (provide 'my_packages)
